e43ccbd1b8b06074e43292cab773b3ee34fc5362
Marco Heisig <marco.heisig@fau.de>
2019-04-06 13:47:29 +0200

    Wrap inline lambdas in a correctly named block.

--- code/implementation-specific/sbcl.lisp
+++ code/implementation-specific/sbcl.lisp
@@ -15,7 +15,8 @@
            (mapcar
             (lambda (specializer-list mask-bit)
               (if mask-bit
-                  (remove-duplicates specializer-list :test #'eq)
+                  (remove (find-class 't)
+                          (remove-duplicates specializer-list :test #'eq))
                   (list (find-class 't))))
             (apply #'mapcar #'list list-of-specializers)
             (generic-function-specializer-profile sgf))))
@@ -42,20 +43,23 @@
            (types (mapcar #'specializer-type specializers)))
       (unless (null applicable-methods)
         (assert (every #'method-sealed-p applicable-methods))
+        (debug-format "~&Creating deftransform for ~S~{ ~S~}~%"
+                      (generic-function-name gf) types)
         `(sb-c:deftransform ,name ((&rest rest) (,@types &rest *))
            (let* ((gensyms (loop for r in rest collect (gensym)))
                   (inline-lambda
                     `(lambda (,@gensyms)
-                       (funcall
-                        ,',(if (and (every #'method-inline-lambda applicable-methods)
-                                    (notany #'method-qualifiers applicable-methods))
-                               (method-inline-lambda (first applicable-methods))
-                               `(load-time-value
-                                 (make-effective-method-using-specializers
-                                  #',(generic-function-name gf)
-                                  (list
-                                   ,@(mapcar #'specializer-load-form specializers)))))
-                        ,@gensyms))))
+                       (block ,',(generic-function-name gf)
+                         (funcall
+                          ,',(if (and (every #'method-inline-lambda applicable-methods)
+                                      (notany #'method-qualifiers applicable-methods))
+                                 (method-inline-lambda (first applicable-methods))
+                                 `(load-time-value
+                                   (make-effective-method-using-specializers
+                                    #',(generic-function-name gf)
+                                    (list
+                                     ,@(mapcar #'specializer-load-form specializers)))))
+                          ,@gensyms)))))
              (debug-format "~&Creating inline lambda:~% ~S~%" inline-lambda)
              inline-lambda))))))
 
