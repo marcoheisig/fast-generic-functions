35c55d597a5b877935b71defebd44ccc9ae6af5b
Marco Heisig <marco.heisig@fau.de>
2020-01-29 12:11:34 +0100

    Allow new sealed methods to be added to sealed GFs on SBCL.

--- code/implementation-specific/sbcl.lisp
+++ code/implementation-specific/sbcl.lisp
@@ -1,35 +1,27 @@
 (in-package #:sealable-metaobjects)
 
-(defmethod seal-metaobject :after
-    ((sgf sealable-generic-function))
-  (eval `(sb-c:defknown ,(generic-function-name sgf) * * () :overwrite-fndb-silently t))
-  (mapc
-   (lambda (call-signature)
-     (eval (make-deftransform sgf call-signature)))
-   (compute-static-call-signatures sgf)))
+(defmethod seal-metaobject :after ((sgf sealable-generic-function))
+  (update-sealed-deftransforms sgf))
 
-(defun make-effective-method-using-prototypes (gf prototypes arity)
-  (let ((applicable-methods (compute-applicable-methods gf prototypes)))
-    (let ((emf (sb-pcl::get-effective-method-function gf applicable-methods)))
-      (typecase emf
-        (sb-pcl::fast-method-call
-         (let ((fn (sb-pcl::fast-method-call-function emf))
-               (pv (sb-pcl::fast-method-call-pv emf))
-               (nm (sb-pcl::fast-method-call-next-method-call emf)))
-           (declare (function fn))
-           (case arity
-             (0 (lambda  () (funcall fn pv nm)))
-             (1 (lambda  (a1) (funcall fn pv nm a1)))
-             (2 (lambda  (a1 a2) (funcall fn pv nm a1 a2)))
-             (3 (lambda  (a1 a2 a3) (funcall fn pv nm a1 a2 a3)))
-             (4 (lambda  (a1 a2 a3 a4) (funcall fn pv nm a1 a2 a3 a4)))
-             (5 (lambda  (a1 a2 a3 a4 a5) (funcall fn pv nm a1 a2 a3 a4 a5)))
-             (6 (lambda  (a1 a2 a3 a4 a5 a6) (funcall fn pv nm a1 a2 a3 a4 a5 a6)))
-             (7 (lambda  (a1 a2 a3 a4 a5 a6 a7) (funcall fn pv nm a1 a2 a3 a4 a5 a6 a7)))
-             (t (lambda (&rest args) (apply fn pv nm args))))))
-        (t
-         (lambda (&rest args)
-           (sb-pcl::invoke-emf emf args)))))))
+(defmethod add-method :after ((sgf sealable-generic-function)
+                              (psm potentially-sealable-method))
+  (when (and (generic-function-sealed-p sgf)
+             (method-sealable-p psm))
+    (seal-method psm)
+    (update-sealed-deftransforms sgf)))
+
+(defun update-sealed-deftransforms (sgf)
+  (let ((name (generic-function-name sgf)))
+    ;; Ensure that the function is known.
+    (eval `(sb-c:defknown ,name * * () :overwrite-fndb-silently t))
+    ;; Remove all existing IR1-transforms.
+    (let ((fun-info (sb-c::fun-info-or-lose name)))
+      (setf (sb-c::fun-info-transforms fun-info) '()))
+    ;; Create an IR1-transform for each static call signature.
+    (mapc
+     (lambda (call-signature)
+       (eval (make-deftransform sgf call-signature)))
+     (compute-static-call-signatures sgf))))
 
 (defun make-deftransform (generic-function static-call-signature)
   (with-accessors ((name generic-function-name)
@@ -42,6 +34,28 @@
                  (generic-function-inline-lambda ',generic-function (length rest) ',prototypes)))
            (debug-format "~&Creating inline lambda:~% ~S~%" inline-lambda)
            inline-lambda)))))
+
+(defun make-effective-method-using-prototypes (gf prototypes arity)
+  (let ((emf (sb-pcl::get-effective-method-function gf (compute-applicable-methods gf prototypes))))
+    (typecase emf
+      (sb-pcl::fast-method-call
+       (let ((fn (sb-pcl::fast-method-call-function emf))
+             (pv (sb-pcl::fast-method-call-pv emf))
+             (nm (sb-pcl::fast-method-call-next-method-call emf)))
+         (declare (function fn))
+         (case arity
+           (0 (lambda  () (funcall fn pv nm)))
+           (1 (lambda  (a1) (funcall fn pv nm a1)))
+           (2 (lambda  (a1 a2) (funcall fn pv nm a1 a2)))
+           (3 (lambda  (a1 a2 a3) (funcall fn pv nm a1 a2 a3)))
+           (4 (lambda  (a1 a2 a3 a4) (funcall fn pv nm a1 a2 a3 a4)))
+           (5 (lambda  (a1 a2 a3 a4 a5) (funcall fn pv nm a1 a2 a3 a4 a5)))
+           (6 (lambda  (a1 a2 a3 a4 a5 a6) (funcall fn pv nm a1 a2 a3 a4 a5 a6)))
+           (7 (lambda  (a1 a2 a3 a4 a5 a6 a7) (funcall fn pv nm a1 a2 a3 a4 a5 a6 a7)))
+           (t (lambda (&rest args) (apply fn pv nm args))))))
+      (t
+       (lambda (&rest args)
+         (sb-pcl::invoke-emf emf args))))))
 
 (defun sb-pcl-symbol-p (symbol)
   (not
