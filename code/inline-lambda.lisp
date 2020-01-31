54777e5d27f14280491358bd74a651a756895387
Marco Heisig <marco.heisig@fau.de>
2020-01-31 17:15:07 +0100

    Finish generic function inline lambda computation.

--- code/inline-lambda.lisp
+++ code/inline-lambda.lisp
@@ -45,10 +45,84 @@
         (setf allow-other-keys-p (and allow-other-keys-p m-allow-other-keys-p))
         (dolist (keyword-info m-keyword)
           (pushnew keyword-info keyword :key #'keyword-info-keyword))))
-    ;; Now replace all bindings of the extended generic function lambda
-    ;; list with gensyms.
-    (multiple-value-setq (required optional rest-var keyword allow-other-keys-p)
-      (anonymize-ordinary-lambda-list
-       (unparse-ordinary-lambda-list required optional rest-var keyword allow-other-keys-p '())))
-    (break)
-    ))
+    ;; We anonymize all bindings of the inline lambda, to prevent
+    ;; accidental variable capture by any of the method lambdas.
+    (let ((anonymized-lambda-list
+            (anonymize-ordinary-lambda-list
+             (unparse-ordinary-lambda-list
+              required optional rest-var keyword allow-other-keys-p '()))))
+      ;; Create the inline lambda.
+      `(lambda ,anonymized-lambda-list
+         (let ((.gf. #',(generic-function-name igf)))
+           (declare (ignorable .gf.))
+           #+sbcl(declare (sb-ext:disable-package-locks common-lisp:call-method))
+           #+sbcl(declare (sb-ext:disable-package-locks common-lisp:make-method))
+           ,(wrap-in-call-method-macrolet
+             (compute-effective-method
+              igf
+              (generic-function-method-combination igf)
+              applicable-methods)
+             anonymized-lambda-list))))))
+
+(defun wrap-in-call-method-macrolet (form lambda-list)
+  `(macrolet ((call-method (method &optional next-methods)
+                (wrap-in-next-methods
+                 (call-inlineable-method method ',lambda-list)
+                 next-methods)))
+     ,form))
+
+(defun wrap-in-next-methods (form arguments next-methods)
+  (if (null next-methods)
+      `(flet ((next-method-p () nil)
+              (call-next-method ()
+                (no-next-method .gf. (sb-pcl:class-prototype 'standard-method))))
+         (declare (ignorable #'next-method-p #'call-next-method))
+         ,form)
+      (wrap-in-next-methods
+       `(flet ((next-method-p () t)
+               (call-next-method ()
+                 ,(first next-methods)))
+          (declare (ignorable #'next-method-p #'call-next-method))
+          ,form)
+       arguments (rest next-methods))))
+
+(defun call-inlineable-method (method lambda-list)
+  (cond ((and (consp method)
+              (eql (first method) 'make-method))
+         (assert (null (rest (rest method))))
+         (second method))
+        ((and (typep method 'potentially-inlineable-method)
+              (method-sealed-p method))
+         (multiple-value-bind (g-required g-optional g-rest-var g-keyword)
+             (parse-ordinary-lambda-list lambda-list)
+           (multiple-value-bind (m-required m-optional m-rest-var m-keyword)
+               (parse-ordinary-lambda-list (method-lambda-list method))
+             (assert (= (length g-required)
+                        (length m-required)))
+             (assert (= (length g-optional)
+                        (length m-optional)))
+             (when (null g-rest-var)
+               (assert (null m-rest-var)))
+             `(funcall
+               ,(method-inline-lambda method)
+               ,@(mapcar #'required-info-variable g-required)
+               ,@(loop for g-info in g-optional
+                       for m-info in m-optional
+                       append
+                       (if (null (optional-info-suppliedp m-optional))
+                           `(,(optional-info-variable g-optional))
+                           `(,(optional-info-variable g-optional)
+                             ,(optional-info-suppliedp g-optional))))
+               ,@(if (null m-rest-var)
+                     `()
+                     `(,m-rest-var))
+               ,@(loop for m-info in m-keyword
+                       for g-info = (find (keyword-info-keyword m-info) g-keyword
+                                          :key #'keyword-info-keyword)
+                       append
+                       (if (null (keyword-info-suppliedp m-optional))
+                           `(,(keyword-info-variable g-optional))
+                           `(,(keyword-info-variable g-optional)
+                             ,(keyword-info-suppliedp g-optional))))))))
+        (t
+         (error "Cannot turn ~S into an inline method call." method))))
