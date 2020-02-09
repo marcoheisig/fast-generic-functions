f614e0b92ac26d388fd1d7a55d5d022a441f6414
Marco Heisig <marco.heisig@fau.de>
2020-02-09 18:12:09 +0100

    Clarify the restrictions on the mutation of sealed metaobjects.

--- code/sealable-metaobject-mixin.lisp
+++ code/sealable-metaobject-mixin.lisp
@@ -8,23 +8,29 @@
   t)
 
 (defmethod seal-metaobject ((metaobject sealable-metaobject-mixin))
-  (debug-format "~&Sealing ~S~%"
-                (etypecase metaobject
-                  (class (class-name metaobject))
-                  (generic-function (generic-function-name metaobject))
-                  (method (list* (if (method-generic-function metaobject)
-                                     (generic-function-name (method-generic-function metaobject))
-                                     metaobject)
-                                 (mapcar #'specializer-type (method-specializers metaobject))))
-                  (t metaobject)))
   (setf (slot-value metaobject '%sealed-p) t))
 
+;; Invoke the primary methods on SEAL-METAOBJECT exactly once.
 (defmethod seal-metaobject :around ((metaobject sealable-metaobject-mixin))
   (unless (metaobject-sealed-p metaobject)
     (call-next-method)))
 
-(defmethod change-class :before
-    ((metaobject sealable-metaobject-mixin) new-class-name &key &allow-other-keys)
-  (when (metaobject-sealed-p metaobject)
-    (error "Attempt to change the class of the sealed metaobject ~S."
-           metaobject)))
+;;; It is an error to change the class of a sealed metaobject.
+(defmethod change-class :around
+    ((metaobject sealable-metaobject-mixin) new-class &key &allow-other-keys)
+  (if (metaobject-sealed-p metaobject)
+      (error "Attempt to change the class of the sealed metaobject ~S."
+             metaobject)
+      (call-next-method)))
+
+;;; It is an error to change any object's class to a sealed metaobject.
+(defmethod update-instance-for-different-class :around
+    (previous (current sealable-metaobject-mixin) &key &allow-other-keys)
+  (error "Attempt to change the class of ~S to the sealable metaobject ~S."
+         previous (class-of current)))
+
+;;; Attempts to reinitialize a sealed metaobject are silently ignored.
+(defmethod reinitialize-initialize :around
+    ((metaobject sealable-metaobject-mixin) &key &allow-other-keys)
+  (unless (metaobject-sealed-p metaobject)
+    (call-next-method)))
