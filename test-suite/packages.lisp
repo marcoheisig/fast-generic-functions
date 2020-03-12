29a70f84a35cbe8158218d65aecc56377847ed2d
Marco Heisig <marco.heisig@fau.de>
2020-03-12 21:28:09 +0100

    Remove superfluous shadowing-import-from clause.

--- test-suite/packages.lisp
+++ test-suite/packages.lisp
@@ -1,5 +1,4 @@
 (cl:in-package #:cl-user)
 
 (defpackage #:sealable-metaobjects-test-suite
-  (:use #:closer-common-lisp #:sealable-metaobjects)
-  (:shadowing-import-from #:sealable-metaobjects #:defgeneric))
+  (:use #:closer-common-lisp #:sealable-metaobjects))
